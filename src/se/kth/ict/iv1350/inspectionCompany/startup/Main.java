a713e9a1161c07ef8ce0f1036ebd3d608b71447e
Joel Wachsler <joel@wachsler.se>
2016-04-29 13:49:14 +0200

    Inital commit!

---
+++ src/se/kth/ict/iv1350/inspectionCompany/startup/Main.java
@@ -0,0 +1,11 @@
+package se.kth.ict.iv1350.inspectionCompany.startup;
+
+/**
+ * This class is used to initialize the program by creating objects needed for the desired functionality.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
